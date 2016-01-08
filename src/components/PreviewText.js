d3c719a80d785837d92211fc5c8902e08853adc1
Rory O’Kane <rory@roryokane.com>
2016-01-08 07:18:47 -0500

    Process newlines correctly in the preview text

--- src/components/PreviewText.js
+++ src/components/PreviewText.js
@@ -1,13 +1,23 @@
 import React, {PropTypes} from 'react';
 
+function convertNewlinesToBrs(multilineText) {
+    return multilineText.split("\n").map((item, index) => (
+        <span key={index}>
+            {item}
+            <br/>
+        </span>
+    ));
+}
+
 let PreviewText = (props) => {
     let escapedFontName = '"' + props.fontName + '"';
+    let formattedText = convertNewlinesToBrs(props.text);
     
     return (
         <div className="section preview">
             <h3>Preview</h3>
             <div className="preview-text" style={{fontFamily: escapedFontName}}>
-                {props.text}
+                {formattedText}
             </div>
         </div>
     );
